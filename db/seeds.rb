926f0729fb62711476158702363ecfc69a564207
fasaldana <fernando_s_98@hotmail.com>
2022-08-10 00:48:04 -0500

    Add back-end endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(text: "Hello 1")
+Message.create(text: "Hello 2")
+Message.create(text: "Hello 3")
+Message.create(text: "Hello 4")
+Message.create(text: "Hello 5")
